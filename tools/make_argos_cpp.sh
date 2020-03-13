0a8c13006568419e2314ddd2e96394dee4febefc
Jan Erik Breimo <jebreimo@gmail.com>
2020-03-13 14:23:01 +0100

    Fix mistake in single-src generation.

--- tools/make_argos_cpp.sh
+++ tools/make_argos_cpp.sh
@@ -3,7 +3,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 python3 "$DIR"/mergecpp.py \
-  "$DIR"/../src/Argos/*.[ch]pp \
+  "$DIR"/../include/Argos/*.[ch]pp \
   -o "$DIR"/../single_src/Argos.hpp
 
 python3 "$DIR"/mergecpp.py \
