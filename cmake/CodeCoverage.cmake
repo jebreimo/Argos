be60bb5f2813d3e3dc244817321f89459e023eef
Jan Erik Breimo <jebreimo@gmail.com>
2020-03-15 14:09:33 +0100

    Add build targets for documentation.

---
+++ cmake/CodeCoverage.cmake
@@ -0,0 +1,17 @@
+# ===========================================================================
+# Copyright © 2020 Jan Erik Breimo. All rights reserved.
+# Created by Jan Erik Breimo on 2020-03-15.
+#
+# This file is distributed under the BSD License.
+# License text is included with the source distribution.
+# ===========================================================================
+cmake_minimum_required(VERSION 3.15)
+
+function(TargetEnableCodeCoverage target isEnabled)
+    if (${isEnabled})
+        if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|.*Clang")
+            target_compile_options(${target} PRIVATE --coverage)
+            target_link_options(${target} PUBLIC --coverage)
+        endif ()
+    endif ()
+endfunction()
---
+++ docs/doxygen/index.rst
@@ -0,0 +1,26 @@
+.. Argos documentation master file, created by
+   sphinx-quickstart on Sun Mar 15 12:51:14 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Argos's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+Docs
+====
+
+.. doxygennamespace:: Argos
+   :members:
