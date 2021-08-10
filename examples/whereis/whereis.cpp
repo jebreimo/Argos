df7aae95aea8f8c1854e2dc728f7a845dbc72693
Jan Erik Breimo <jebreimo@gmail.com>
2021-08-10 23:28:57 +0200

    Finish whereis.

--- examples/whereis/whereis.cpp
+++ examples/whereis/whereis.cpp
@@ -13,6 +13,8 @@ const char VERSION[] = "1.0.0";
 
 int main(int argc, char* argv[])
 {
+    auto pathEnv = std::getenv("PATH");
+
     using namespace Argos;
     auto args = ArgumentParser(argv[0])
         .about("Displays the path (or paths) where the given commands are"
@@ -24,6 +26,7 @@ int main(int argc, char* argv[])
         .add(Option{"-v", "--verbose"}
             .text("Show additional information."))
         .add(Option{"-p", "--paths"}.argument("<PATH>[:<PATH>]...")
+            .initialValue(pathEnv ? pathEnv : std::string())
             .text("Search the given path or paths rather than the ones in"
                   " the PATH environment variable."))
         .add(Option{"--version"}
@@ -35,5 +38,20 @@ int main(int argc, char* argv[])
             .type(OptionType::STOP)
             .text("Display the program version and exit."))
         .parse(argc, argv);
+
+    auto dirs = args.values("--paths").split(':').asStrings();
+    auto verbose = args.value("--verbose").asBool();
+
+    for (const auto& dir : dirs)
+    {
+        for (auto command : args.values("COMMAND").asStrings())
+        {
+            auto path = std::filesystem::path(dir) / command;
+            if (std::filesystem::exists(path))
+                std::cout << path.string() << '\n';
+            else if (verbose)
+                std::cout << "not found: " << path.string() << '\n';
+        }
+    }
     return 0;
 }
