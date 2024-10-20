b4994216cb0a9a93a54f6b1792b02c53555b2e33
Jan Erik Breimo <jebreimo@gmail.com>
2024-10-20 14:12:05 +0200

    Fix bug in ParsedArguments The all_options, all_arguments and all_commands methods returned the objects belonging to the top-level command (ArgumentParser), not the current sub-command.

--- tests/ArgosTest/test_ParsedArguments.cpp
+++ tests/ArgosTest/test_ParsedArguments.cpp
@@ -45,3 +45,38 @@ TEST_CASE("Test filter argc/argv")
         REQUIRE(std::string(v[3]) == "text");
     }
 }
+
+TEST_CASE("Check options of arguments of main command and sub-commands")
+{
+    using namespace argos;
+    auto args = ArgumentParser()
+        .add(Opt("-f", "--flag"))
+        .add(Cmd("cmd")
+            .add(Opt("-o", "--option"))
+            .add(Arg("ARG")))
+        .parse({"-f", "cmd", "-o", "arg"});
+    auto all_opts = args.all_options();
+    REQUIRE(all_opts.size() == 2);
+    REQUIRE(all_opts[0]->flags().size() == 2);
+    REQUIRE(all_opts[0]->flags()[0] == "-f");
+    REQUIRE(all_opts[0]->flags()[1] == "--flag");
+    REQUIRE(all_opts[1]->flags().size() == 2);
+    REQUIRE(all_opts[1]->flags()[0] == "-h");;
+    REQUIRE(all_opts[1]->flags()[1] == "--help");
+    REQUIRE(args.all_arguments().empty());
+    auto cmds = args.subcommands();
+    REQUIRE(cmds.size() == 1);
+    auto cmd = cmds[0];
+    REQUIRE(cmd.name() == "cmd");
+    all_opts = cmd.all_options();
+    REQUIRE(all_opts.size() == 2);
+    REQUIRE(all_opts[0]->flags().size() == 2);
+    REQUIRE(all_opts[0]->flags()[0] == "-o");
+    REQUIRE(all_opts[0]->flags()[1] == "--option");
+    REQUIRE(all_opts[1]->flags().size() == 2);
+    REQUIRE(all_opts[1]->flags()[0] == "-h");
+    REQUIRE(all_opts[1]->flags()[1] == "--help");
+    auto all_args = cmd.all_arguments();
+    REQUIRE(all_args.size() == 1);
+    REQUIRE(all_args[0]->name() == "ARG");
+}
